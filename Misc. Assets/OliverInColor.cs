c43b83f8073eaa1faae0527d72e0cd44b0d2124b
Zachary Segall <zhhsegall@gmail.com>
2017-03-06 13:03:19 -0600

    Add assets

---
+++ Misc. Assets/OliverInColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OliverInColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			GetComponent<Renderer> ().material.color = Color.red;
+		}
+		if (Input.GetKeyDown(KeyCode.G))
+		{
+			GetComponent<Renderer>().material.color = Color.green;
+		}
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			GetComponent<Renderer>().material.color = Color.blue;
+		}
+	}
+}
